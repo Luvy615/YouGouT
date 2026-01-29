2ce60197dc102d0e65a847856519b7bb42a286fa
Luvy <2448705562@qq.com>
2026-01-29 15:33:33 +0800

    Personalized recommendations for e-commerce

---
+++ 2.0/src/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:60734655a70d7b8cd4715352bba79ef829f1514204bdb65ade5668bf28367d9d
+size 23486
---
+++ 2.0/src/langchain_agent.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:485a62375a8d9319f0d7e040d788f4fdac636fb0ae05fbaa37eac49168882dbf
+size 5093
